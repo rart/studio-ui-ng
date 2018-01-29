21eaefac58693bf9a1d33013f3d149b9f7e4dd98
Roy Art <royart@me.com>
2018-01-29 17:01:20 +0100

    Further progress on schedule review component

--- src/app/components/asset-review/schedule-review.component.ts
+++ src/app/components/asset-review/schedule-review.component.ts
@@ -1,21 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ReviewBase } from '../../classes/review-base.class';
+import { Component } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
 import { ActivatedRoute } from '@angular/router';
 import { AppState } from '../../classes/app-state.interface';
+import { PublishReviewComponent } from './publish-review.component';
 
 @Component({
   selector: 'std-schedule-review',
   templateUrl: './schedule-review.component.html',
   styleUrls: ['./schedule-review.component.scss']
 })
-export class ScheduleReviewComponent extends ReviewBase {
-
-  constructor(store: NgRedux<AppState>, route: ActivatedRoute) {
-    super(store, route);
-
-
-
-  }
+export class ScheduleReviewComponent extends PublishReviewComponent {
 
 }
